f197bee981141226ad0a0d9c23bf6a88ddff3bd0
jinwooseok <jinus7949@naver.com>
2024-09-30 21:43:27 +0900

    9.30

---
+++ swea/Solution_3282.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+public class Solution_3282 {
+    /*
+     * N개의 물건, K 부피만큼 넣을 수 있는 가방
+     * N개가
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t=1;t<=T;t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            int[][] dp = new int[N+1][K+1];
+            for (int i=1;i<N+1;i++) {
+                st = new StringTokenizer(br.readLine());
+                int start=Integer.parseInt(st.nextToken());
+                int value=Integer.parseInt(st.nextToken());
+                for (int j=1;j<K+1;j++) {
+                    if (j-start<0) {
+                        dp[i][j] = dp[i-1][j];
+                    } else {
+                        dp[i][j]=Math.max(dp[i-1][j-start]+value,dp[i-1][j]);
+                    }
+                }
+            }
+            System.out.println("#"+t+" "+dp[N][K]);
+        }
+
+    }
+
+}

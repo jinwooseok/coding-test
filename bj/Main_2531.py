30a8968346afcbf4e43d4eeee2b46bb7e9e061ec
jinwooseok <jinus7949@naver.com>
2024-10-12 13:21:39 +0900

    chore

--- bj/Main_2531.py
+++ bj/Main_2531.py
@@ -12,15 +12,16 @@
 즉, 가능한 경우는 k개의 종류를 먹거나 k+1개를 먹거나
 연속되게 한바퀴 돌면서 k개를 다 먹을 수 있는지 보자
 '''
-import sys
-from collections import defaultdict
-input = sys.stdin.readline
-sushiCnt, sushiTypeCnt, seqDishCnt, couponNum = map(int, input().split())
-belt = [0]*sushiCnt
-sushiTypes = defaultdict(int)
+# import sys
+# from collections import defaultdict
+# sys.setrecursionlimit(10**6)
+# input = sys.stdin.readline
+# sushiCnt, sushiTypeCnt, seqDishCnt, couponNum = map(int, input().split())
+# belt = [0]*sushiCnt
+# sushiTypes = defaultdict(bool)
 
-for i in range(sushiCnt):
-    belt[i] = int(input())
+# for i in range(sushiCnt):
+#     belt[i] = int(input())
 
 # def dfs(belt, sushiTypes, startIndex, lastIndex, couponNum, maxCnt):
 #     result = 0
@@ -38,28 +39,43 @@ for i in range(sushiCnt):
 #             result=dfs(belt, sushiTypes, (startIndex+1)%len(belt), lastIndex, couponNum, maxCnt)
 #     return max(maxCnt,result)
 
-maxCnt = 0
+# maxCnt = 0
 # for i in range(sushiCnt):
 #     maxCnt = max(maxCnt,dfs(belt, sushiTypes, i, (i+seqDishCnt)%sushiCnt, couponNum, 0))
-
+# print(maxCnt)
 # 초기 윈도우 설정
+
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+sushiCnt, sushiTypeCnt, seqDishCnt, couponNum = map(int, input().split())
+belt = [0]*sushiCnt
+sushiTypes = defaultdict(int)
+
+for i in range(sushiCnt):
+    belt[i] = int(input())
+maxCnt = 0
+#초기 설정
 for i in range(seqDishCnt):
     sushiTypes[belt[i]] += 1
 
 maxCnt = len(sushiTypes)
-if sushiTypes[couponNum] == 0:
+if couponNum not in sushiTypes.keys():
     maxCnt += 1
-    
+#print(sushiTypes, maxCnt)
+#슬라이딩 시작
 for i in range(1, sushiCnt):
-    sushiTypes[belt[(i + seqDishCnt-1)%sushiCnt]] += 1
+    sushiTypes[belt[(i + seqDishCnt - 1)%sushiCnt]] += 1
     
     sushiTypes[belt[i-1]] -= 1
+    
     if sushiTypes[belt[i-1]] == 0:
         del sushiTypes[belt[i-1]]
-    curCnt = len(sushiTypes)
-    if sushiTypes[couponNum] == 0:
-        curCnt += 1
     
+    curCnt = len(sushiTypes)
+    if couponNum not in sushiTypes.keys():
+        curCnt += 1
+    #print(sushiTypes, curCnt)
     maxCnt = max(maxCnt, curCnt)
     
 print(maxCnt)
